667c154167ca39ffa12db3de1aa4c02f279baaad
Fiona Dcruz <fionavdcruz@users.noreply.github.com>
2017-03-26 23:19:10 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+         <title>Flavour</title>
+     <script src="https://use.fontawesome.com/c5f902c4cd.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+</head>
+    <body>
+        <header>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="overlay">
+                        <nav id="menu-transparent" class="navbar navbar-inverse navbar-fixed-top">
+                            <div class="container">
+                              <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+                                  <span class="sr-only">Toggle navigation</span>
+                                  <span class="icon-bar"></span>
+                                  <span class="icon-bar"></span>
+                                  <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="#">Flavour</a>
+                              </div>
+                              <div id="navbar3" class="navbar-collapse collapse">
+                                <ul id="menu-right" class="nav navbar-nav navbar-right" contenteditable="true">
+                                  <li><a href="#">About</a></li>
+                                  <li><a href="#">Menu</a></li>
+                                  <li><a href="#">Gallery</a></li>
+                                  <li><a href="#">Chefs</a></li>
+                                  <li><a href="#">Contact</a></li>
+                                </ul>
+                              </div>
+                            </div>
+                          </nav>
+                      <div class="header-cover">
+                       </div>
+                    </div>
+                 </div>
+            </div>
+        </header> 
+        <section>
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-6" >
+                  <div class="left-section" >
+                    <img class="img-responsive" src="imgs/about-1.jpg" alt="our restaurant">
+                  </div>
+              </div>
+               <div class="col-sm-6">
+                   <div class="right-section">
+                   <h2>Our Restaurant</h2>
+                   <hr>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+        <section>
+        <div class="container">
+          <div class="row">
+              <div class="col-sm-6">
+                <div class="left-about">
+                  <h2>Only The Freshest Food</h2>
+                  <hr>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</p>    
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="right-about">
+                  <img class="img-responsive" src="imgs/about-2.jpg" alt="fresh food">
+                </div>
+              </div>
+            </div>    
+        </div>
+        </section>
+        <section>
+         <div class="container-fluid">
+              <div class="row">
+                <div class="try">
+               <div class="overlay">
+                <div class="menu-parralax">
+                  <h2>Menu</h2>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed.</p>
+                </div>
+                 </div> 
+              </div>
+            </div>
+         </div>
+        </section>
+        <section>
+        <div class="container">
+           <div class="row">
+           <p>The tabs content comes here</p> 
+           </div>    
+        </div>
+        </section>
+        <section>
+        <div class="container-fluid">
+           <div class="row">
+               
+           </div>    
+        </div>
+        </section>
+        <section>
+            <div class="container">
+              <div class="row">
+                 <div class="col-sm-12">
+                     <div class="chefs">
+                          <h2>Meet Our Chefs</h2>
+                             <hr>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+                      </div>
+                 </div>
+               </div>
+                <div class="row">
+                  <div class="col-sm-4" style="background-color:">
+                    <div class="chef-imgs">
+                    <img class="img-responsive animated bounce" src="../Flavour/imgs/chef01.jpg" alt="chef one">
+                        <h3>Benjamin Doe</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam.</p>
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                      <div class="chef-imgs">
+                     <img class="img-responsive" src="../Flavour/imgs/chef02.jpg" alt="chef two" >
+                        <h3>Christina Doe</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam.</p>
+                      </div>
+                  </div>
+                  <div class="col-sm-4">
+                      <div class="chef-imgs">
+                        <img class="img-responsive" src="../Flavour/imgs/chef03.jpg" alt="chef three">
+                           <h3>Robert Doe</h3>
+                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam.</p>
+                      </div>
+                  </div>
+                </div>
+            </div>
+        </section>
+        <section>
+         <div class="container-fluid">
+              <div class="row">
+                <div class="try">
+               <div class="overlay">
+                <div class="reservation-para">
+                  <h2>Want to make a reservation? <span>Call 1-887-654-3210</span></h2>
+                </div>
+                 </div> 
+              </div>
+            </div>
+          <div class="row">
+             <div class="col-sm-6 rest-img">
+                      </div>
+          <div class="col-sm-6 rest-map">
+              <div class="map-responsive">
+             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d492481.04915127676!2d73.7347356361224!3d15.348939467421491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbfba106336b741%3A0xeaf887ff62f34092!2sGoa!5e0!3m2!1sen!2sin!4v1487618114713" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+          </div> 
+          </div>    
+        </div>
+        </section>
+        <section>
+     <div id="footer">
+        <div class="container">
+         <div class="row">
+          <div class="col-sm-4">
+            <h3>Address</h3>
+              <hr>
+              <p>321 Awesome Street</p>
+               <p>New York, NY 17022</p>
+          </div>
+          <div class="col-sm-4">
+             <h3>Opening Hours</h3>
+              <hr>
+              <p>Mon-Thurs: 10:00 AM - 11:00 PM</p>
+               <p>Fri-Sun: 11:00 AM - 02:00 AM</p>
+          </div> 
+          <div class="col-sm-4">
+             <h3>Contact Info</h3>
+              <hr>
+               <p>Phone: +1 123 456 1234</p>
+               <p>Email: info@company.com</p>
+               </div> 
+             </div>
+         </div>
+          </div>
+         <div class="container-fluid">
+          <div class="row" id="copyright">
+              <ul>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+              </ul>
+              <p> &copy 2016 Flavour. All rights reserved.</p>
+            </div>
+         </div>
+     
+        </section>
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
